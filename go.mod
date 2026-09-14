47aebb342f1486b019d7292380bb5050dc306430
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-09-14 14:36:32 +0545

    feat: update dependencies and improve project structure
    - Bump Go version to 1.26.0 in go.mod
    - Update dependencies in go.mod and go.sum, including:
      - github.com/chzyer/readline v1.5.1
      - golang.org/x/crypto v0.57.0
      - golang.org/x/sys v0.48.0
      - golang.org/x/term v0.46.0
    - Add a justfile for build, test, lint, and clean commands
    - Refactor main.go to use io.ReadAll and os.WriteFile
    - Enhance the renovate.json configuration for better dependency management
    - Create a gitignorer file to manage ignored files

--- go.mod
+++ go.mod
@@ -1,13 +1,15 @@
 module github.com/rubiin/gitignorer
 
-go 1.16
+go 1.26.0
+
+require (
+	github.com/gernest/wow v0.1.0
+	github.com/manifoldco/promptui v0.9.0
+)
 
 require (
 	github.com/chzyer/readline v1.5.1 // indirect
-	github.com/gernest/wow v0.1.0
-	github.com/juju/ansiterm v1.0.0 // indirect
-	github.com/manifoldco/promptui v0.9.0
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/crypto v0.57.0 // indirect
+	golang.org/x/sys v0.48.0 // indirect
+	golang.org/x/term v0.46.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,14 +5,14 @@ import (
 	"github.com/gernest/wow"
 	"github.com/gernest/wow/spin"
 	"github.com/manifoldco/promptui"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
 )
 
-func getEnvironments() []string{
+func getEnvironments() []string {
 	loadingSpinner := wow.New(os.Stdout, spin.Get(spin.Dots), " Getting environments")
 	loadingSpinner.Start()
 	resp, err := http.Get("https://www.gitignore.io/api/list")
@@ -20,55 +20,56 @@ func getEnvironments() []string{
 		log.Fatalln(err)
 	}
 
+	defer resp.Body.Close()
+
 	//We Read the response body on the line below
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	//Convert the body to type string
 	sb := string(body)
-	items := strings.Split(sb,",")
+	items := strings.Split(sb, ",")
 	loadingSpinner.Stop()
-
 	return items
 }
 
-func writeFile(language string){
+func writeFile(language string) {
 	addingSpinner := wow.New(os.Stdout, spin.Get(spin.Dots), "Adding gitignore")
 
-	giResponse, err := http.Get(fmt.Sprintf("https://www.gitignore.io/api/%s",language))
+	giResponse, err := http.Get(fmt.Sprintf("https://www.gitignore.io/api/%s", language))
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer giResponse.Body.Close()
+
 	//We Read the response body on the line below.
-	giBody, err := ioutil.ReadAll(giResponse.Body)
+	giBody, err := io.ReadAll(giResponse.Body)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	//Convert the body to type string
 	addingSpinner.Stop()
-	err = ioutil.WriteFile(".gitignore", giBody, 0644)
+	err = os.WriteFile(".gitignore", giBody, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Successfully created gitignore for %s\n", language)
 }
 
-
-
 func main() {
 	items := getEnvironments()
 	prompt := promptui.Select{
-		Label: "Select environment",
-		Items: items,
-		StartInSearchMode : true,
-		Searcher : func(input string, index int) bool {
-		pepper := items[index]
-		name := strings.Replace(strings.ToLower(pepper), " ", "", -1)
-		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+		Label:             "Select environment",
+		Items:             items,
+		StartInSearchMode: true,
+		Searcher: func(input string, index int) bool {
+			pepper := items[index]
+			name := strings.Replace(strings.ToLower(pepper), " ", "", -1)
+			input = strings.Replace(strings.ToLower(input), " ", "", -1)
 
-		return strings.Contains(name, input)
-	},
+			return strings.Contains(name, input)
+		},
 	}
 
 	_, result, err := prompt.Run()
